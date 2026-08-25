64341c4e788cf9f248595f2a334d1a471aee2789
Shrawan <shrawankick@gmail.com>
2026-08-26 00:00:07 +0530

    feat: refactor SmokeTests to use ApiClient wrapper

--- ApiTests/SmokeTests.cs
+++ ApiTests/SmokeTests.cs
@@ -1,34 +1,29 @@
-using Microsoft.Playwright;
+using ApiTests.Core;
 using NUnit.Framework;
 
 namespace ApiTests;
-public class Smoketests
+
+public class SmokeTests
 {
-    private IPlaywright _playwright;
-    private IAPIRequestContext _apiContext;
+    private ApiClient _apiClient = null!;
 
     [SetUp]
     public async Task Setup()
     {
-        _playwright = await Playwright.CreateAsync();
-        _apiContext = await _playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions
-        {
-             BaseURL = "https://dummyjson.com"         
-        });
+        _apiClient = new ApiClient();
+        await _apiClient.InitializeAsync("https://dummyjson.com");
     }
 
     [Test]
     public async Task GetProducts_ReturnsOk()
     {
-        var response = await _apiContext.GetAsync("/products");
+        var response = await _apiClient.Context.GetAsync("/products");
         Assert.That(response.Ok, Is.True);
     }
 
     [TearDown]
     public async Task TearDown()
     {
-        await _apiContext.DisposeAsync();
-        _playwright.Dispose();
+        await _apiClient.DisposeAsync();
     }
 }
-
